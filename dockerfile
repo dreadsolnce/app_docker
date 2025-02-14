9a0971a8f3579a5f4ea3dd99937e652069e2807c
Владимир <you@example.com>
2025-02-14 14:08:45 +0300

    first commit

---
+++ dockerfile
@@ -0,0 +1,4 @@
+FROM php:8.3-apache
+
+COPY ./site /var/www/html
+
---
+++ site/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Тестовый сайт</title>
+  </head>
+  <body>
+  <h1>Добро пожаловать на тестовый сайт !</h1>
+
+  <p> App backend <?=gethostname()?> </p>
+
+    <img src="/images/tux.jpg" />
+  </body>
+</html>
+
+
